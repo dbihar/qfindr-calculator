fc8c575ed24740049a7cc8f5832a17942c426acb
Daniel Bihar <bihar.daniel@gmail.com>
2022-02-23 15:04:26 +0100

    first

---
+++ calculator.js
@@ -0,0 +1,27 @@
+const mathjs = require('mathjs')
+const numeral = require('numeral')
+
+module.exports = (pluginContext) => {
+  return {
+    respondsTo: (query) => {
+      return query.match(/\d/)
+    },
+    search: (query, env = {}) => {
+      return new Promise((resolve, reject) => {
+        const answer = mathjs.eval(query.replace(/\,/g,"")).toString()
+        const value = answer.match(/[\d.]+/g).join('')
+        const title = answer.replace(/\d+/, (v) => {
+          return numeral(v).format('0,0')
+        })
+        resolve([
+          {
+            icon: 'fa-calculator',
+            title: title,
+            subtitle: 'Select item to copy the value to the clipboard.',
+            value: value,
+          },
+        ])
+      })
+    },
+  }
+}
---
+++ readme.md
@@ -0,0 +1,30 @@
+## Calculator
+
+A basic calculator plugin written for zazu.
+
+## Usage
+
+Open Zazu and type in any math equation you can think of. Here are a few
+examples:
+
+* `13lb to oz`
+* `pi * 3`
+* `12 * 12`
+
+If you click on the result it will copy the result to your clipboard.
+
+## Installing
+
+Add `tinytacoteam/zazu-calculator` inside of `plugins` block of your  `~/.zazurc.json` file.
+
+~~~ json
+{
+  "plugins": [
+    "tinytacoteam/zazu-calculator"
+  ]
+}
+~~~
+
+## Screenshots
+
+![calculator](./screenshot.png)
